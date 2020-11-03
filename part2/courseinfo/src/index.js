357a4402a4bdd12dc6b01709bd93b70893647b91
ting-chen-cn <chenting_cn@foxmail.com>
2020-11-03 12:51:41 +0200

    Exercises 2.1

---
+++ part2/courseinfo/src/index.js
@@ -0,0 +1,67 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const Header = (props) => {
+  return (
+    <div>
+      <h1>{props.course}</h1>
+    </div>
+  );
+};
+
+const Content = (props) => {
+  return (
+    <div>
+      <Part name={props.parts[0].name} exercise={props.parts[0].exercises} />
+      <Part name={props.parts[1].name} exercise={props.parts[1].exercises} />
+      <Part name={props.parts[2].name} exercise={props.parts[2].exercises} />
+    </div>
+  );
+};
+
+const Part = (props) => {
+  return (
+    <div>
+      <>
+        {props.name} {props.exercise}
+      </>
+    </div>
+  );
+};
+
+const Course = ({ course }) => {
+  return (
+    <div>
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+    </div>
+  );
+};
+
+const App = () => {
+  const course = {
+    id: 1,
+    name: "Half Stack application development",
+    parts: [
+      {
+        name: "Fundamentals of React",
+        exercises: 10,
+        id: 1,
+      },
+      {
+        name: "Using props to pass data",
+        exercises: 7,
+        id: 2,
+      },
+      {
+        name: "State of a component",
+        exercises: 14,
+        id: 3,
+      },
+    ],
+  };
+
+  return <Course course={course} />;
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
