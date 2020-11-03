adaac00e5b9fac19b4d708c5a1dbd288871f88a1
Yang Nan <yangnan@Yangs-MacBook-Pro.local>
2020-11-04 01:51:29 +0200

    Exercises 2.6

---
+++ part2/phonebook/src/App.js
@@ -0,0 +1,41 @@
+import React, { useState } from "react";
+
+const App = () => {
+  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [newName, setNewName] = useState("");
+
+  const addPerson = (event) => {
+    event.preventDefault();
+    const noteObject = {
+      name: newName,
+    };
+    setPersons(persons.concat(noteObject));
+    setNewName("");
+  };
+  const handlePersonChange = (event) => {
+    console.log(event.target.value);
+    setNewName(event.target.value);
+  };
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addPerson}>
+        <div>
+          name: <input value={newName} onChange={handlePersonChange} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <ul>
+        {persons.map((note) => (
+          <li key={note.name}>{note.name}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default App;
