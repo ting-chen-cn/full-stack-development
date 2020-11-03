78a2cc64a42d2f3e3fb5403c7d42dedc0b1abc10
ting-chen-cn <chenting_cn@foxmail.com>
2020-11-03 13:58:21 +0200

    Exercises 2.5

--- part2/courseinfo/src/index.js
+++ part2/courseinfo/src/index.js
@@ -1,60 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
-const Header = (props) => {
-  return (
-    <div>
-      <h2>{props.course}</h2>
-    </div>
-  );
-};
-
-const Content = (props) => {
-  return (
-    <div>
-      {props.parts.map((note) => (
-        <Part key={note.id} name={note.name} exercise={note.exercises} />
-      ))}
-    </div>
-  );
-};
-
-const Part = (props) => {
-  return (
-    <div>
-      <>
-        {props.name} {props.exercise}
-      </>
-    </div>
-  );
-};
-const Total = (props) => {
-  const initialValue = 0;
-  const total = props.parts.reduce(
-    (accumulator, currentValue) => accumulator + currentValue.exercises,
-    initialValue
-  );
-
-  return (
-    <div>
-      <b>
-        total of &nbsp;{""}
-        {total}
-        &nbsp; exercises
-      </b>
-    </div>
-  );
-};
-
-const Course = ({ course }) => {
-  return (
-    <div>
-      <Header course={course.name} />
-      <Content parts={course.parts} />
-      <Total parts={course.parts} />
-    </div>
-  );
-};
+import Course from "./components/Course";
 
 const App = () => {
   const courses = [
