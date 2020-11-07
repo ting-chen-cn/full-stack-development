14b53d67d31cb4e2948370edb19e3c1af5096e75
Yang Nan <yangnan@Yangs-MacBook-Pro.local>
2020-11-07 22:31:44 +0200

    Exercises 2.11

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 const PersonForm = ({
   addPerson,
@@ -47,16 +48,20 @@ const Persons = ({ searchedPerson }) => {
   );
 };
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-123456" },
-    { name: "Ada Lovelace", number: "39-44-5323523" },
-    { name: "Dan Abramov", number: "12-43-234345" },
-    { name: "Mary Poppendieck", number: "39-23-6423122" },
-  ]);
+  const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
   const [newSearchPerson, setSearchPerson] = useState("");
   const [searchedPerson, setSearched] = useState(persons);
+  useEffect(() => {
+    console.log("effect");
+    axios.get("http://localhost:3001/persons").then((response) => {
+      console.log("promise fulfilled");
+      setPersons(response.data);
+      setSearched(response.data);
+    });
+  }, []);
+  console.log("render", persons.length, "notes");
 
   const addPerson = (event) => {
     event.preventDefault();
